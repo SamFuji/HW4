df44c000346dfb7859986852eedfca5b8d90d551
SamFuji <samfuji174@mail.ru>
2022-08-21 23:34:37 +0500

    task 27 ended

--- Program.cs
+++ Program.cs
@@ -2,16 +2,30 @@
 
 // Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
 
-Console.WriteLine();
-// Задача 23
+// Console.WriteLine();
+// // Задача 23
+
+// Console.WriteLine("Enter the number: ");  //Вводим число
+// int number = Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine("Enter the power of number: "); //Вводим требуемую степень
+// int power = Convert.ToInt32(Console.ReadLine());
+
+// int numPow =Convert.ToInt32( Math.Pow (number, power)); // Используем функцию возведения числа в степень
+// Console.WriteLine($"The number {number} in power of {power}: {numPow}"); // Выводим результат
+
+
+// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
 
 Console.WriteLine("Enter the number: ");
-int number = Convert.ToInt32(Console.ReadLine());
 
-Console.WriteLine("Enter the power of number: ");
-int power = Convert.ToInt32(Console.ReadLine());
+int number = int.Parse(Console.ReadLine());
 
-int numPow =Convert.ToInt32( Math.Pow (number, power));
-Console.WriteLine($"The number {number} in power of {power}: {numPow}");
+int summ = 0;
 
-
+while (number > 0) 
+    {
+    summ +=  number % 10; //Остаток от деления на 10 складываем  с суммой, изначально равной нулю
+    number /= 10; //Уменьшаем разряд числа, пользуясь преимуществами группы "int". Если был double, перед делением добавил бы строчку number -= (number % 10);
+    }
+    Console.WriteLine(summ); 
