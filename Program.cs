88e74d75b7b6c6770ae5eb1e77c422444f620897
SamFuji <samfuji174@mail.ru>
2022-08-21 22:44:09 +0500

    task 25 ended

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Clear();
+
+// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+Console.WriteLine();
+// Задача 23
+
+Console.WriteLine("Enter the number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Enter the power of number: ");
+int power = Convert.ToInt32(Console.ReadLine());
+
+int numPow =Convert.ToInt32( Math.Pow (number, power));
+Console.WriteLine($"The number {number} in power of {power}: {numPow}");
+
+
